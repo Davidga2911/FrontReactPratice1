a6cf0054d804db7c46f0f1a94309652679c9db2c
David Gallegos <david291192@hotmail.com>
2021-05-23 14:05:48 -0500

    numbers Functions
    numbers Functions only Front

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 //impotación
 import React from 'react'
-import Button from './components/Button'
+import Functions from './components/Functions'
 import MathOperations from "./components/MathOperations"
+import Numbers from "./components/Numbers"
 import Result from './components/Result'
 import './App.css'
 
@@ -9,30 +10,26 @@ import './App.css'
 //generación de la función del componente
 //arrow Function
 const App = () => {
-
-    const clickHandlerFunction = text => console.log(text)
     //lo que ejecuta la función
     console.log("Renderizacionde App")
     return (
     <main className='react-calculator'>
         <Result  /> 
         <div className='numbers'>
-            <Button text="1" clickHandler={clickHandlerFunction}/>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
+            <Numbers onClickNumber={number =>
+            console.log(number)}/>
         </div>
-        <div className="functions">
-            <button>clear</button>
-            <button>remove</button>
-        </div>
-            <MathOperations/>
+            <Functions onContentClear={clear => 
+            console.log("Conten Clear")}
+            onDelete={() =>
+            console.log("onDelete")}/>
+
+            <MathOperations onClickOpertaion={operation => 
+                console.log("Operation:", operation)
+            }
+                onClickEqual= {equal => 
+                console.log("Equal", equal )
+            }/>
     </main>)
     }
 //exportación
